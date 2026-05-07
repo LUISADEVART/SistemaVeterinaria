85fad1f33f0f69a36b24a6a2f5313e4ca668f29e
LUISA <luisamdev@gmail.com>
2026-05-07 18:36:16 -0500

    IMPLEMENTACION DE CLASE PERSONA CON CLIENTE
    cree la clase cliente heredado de persona, luego la implemente a la main en el menu

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
@@ -6,7 +7,8 @@ public class Main {
 
         Mascota m1 = new Mascota("Evangeline", 11, "perro", "Malamute de Alaska", 27.23f);
 
-        Consulta c1 = new Consulta("23/04/2026", "19:00",  "Le duele la oreja", "Otitis", "Gritin cada 8 horas", m1);
+        Consulta consulta1 = new Consulta("23/04/2026", "19:00",  "Le duele la oreja", "Otitis", "Gritin cada 8 horas", m1);
+        Cliente cliente1 = new Cliente("Luisa" ,"123456", "p sherman calle wallaby 47 sidney", "natural", "10001345", LocalDate.of(2026, 5, 7), 7);
 
     int opcion;
     Scanner infoIngresada =  new Scanner(System.in);
@@ -18,34 +20,33 @@ public class Main {
 
             switch(opcion){
                 case 1:
-                    System.out.println("o=o=o=> Persona <=o=o=o");
-
+                    System.out.println("o=o=o=> CLIENTE <=o=o=o");
+                    cliente1.mostrarInfo();
                 break;
                 case 2:
+                    System.out.println("o=o=o=> MASCOTA <=o=o=o");
                     m1.MostrarInfo();
                 break;
                 case 3:
-                    System.out.println("o=o=o=> Consulta <=o=o=o");
-                    c1.MostrarInfo();
+                    System.out.println("o=o=o=> CONSULTA <=o=o=o");
+                    consulta1.MostrarInfo();
                 break;
-                case 5:
+                case 4:
                 infoIngresada.close();
                 break;
             }
         }
-        while(opcion !=5);
+        while(opcion !=4);
     }
     public static void Menu(){
         System.out.println("MENU VETERINARIA LA PEZUÑA");
-        System.out.println("Opción 1 : Persona");
+        System.out.println("Opción 1 : Cliente");
         System.out.println("Opción 2 : Mascota");
         System.out.println("Opción 3 : Consulta");
         System.out.println("Opción 4 Salir del programa Vet");
-        System.out.println("Luisa la mansa paloma");
     }
 }
 
-     
 
 
 
--- Persona.java
+++ Persona.java
@@ -1,21 +1,51 @@
 public abstract class Persona {
 
     private String nombre;
-    private int telefono;
+    private String telefono;
     private String direccion;
     private String tipoPersona;
 
-    public Persona(String nombre, int telefono, String direccion) {
+    public Persona(String nombre, String telefono, String direccion, String tipoPersona) {
         this.nombre = nombre;
         this.telefono = telefono;
         this.direccion = direccion;
         this.tipoPersona = tipoPersona;
     }
+ 
 
-    public void MostrarInfo() {
-        System.out.println("El nombre del usuario ingresado es: " + nombre);
-        System.out.println("El telefono es: " + telefono);
-        System.out.println("La direccion es: " + direccion);
-        System.out.println("El tipo de persona es " + tipoPersona);
+    public abstract void mostrarInfo();
+ //geters
+    public String getNombre(){
+    return nombre;
     }
+    public String getTelefono(){
+    return telefono;
+    }
+
+    public String getDireccion(){
+    return direccion;
+    }
+
+    public String getTipoPersona(){
+    return tipoPersona;
+    }
+
+//seters
+    public void Setnombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void SetTelefono(String telefono){
+        this.telefono = telefono;
+
+    }
+    public void SetDireccion(String direccion){
+        this.direccion = direccion;
+
+    }
+        public void SetTipoPersona(String tipoPersona){
+        this.tipoPersona = tipoPersona;
+
+    }
+
 }
